4865cbf56be6dc761f83294e30f0ff46bba0dd05
HirotakaInoue <ih0320ih@gmail.com>
2025-12-06 23:33:53 +0900

    2-1, 文と式は”結果”の有無。式は結果が合って、文には結果がない。

---
+++ src/section2/2-1 .ts
@@ -0,0 +1,11 @@
+const greeting = "hello,";
+const target = greeting;
+const text = greeting + "world!";
+console.log(text);
+
+
+const num: number = 8;
+
+if (num < 10) {
+  console.log("numは10未満です");
+}
